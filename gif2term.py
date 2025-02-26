c8b21ffa302c2216846c86587cffda4b9bc83e89
Doggich <dogglyg01@gmail.com>
2025-02-26 20:38:55 +0300

    improve code efficiency, add requirements.txt

--- gif2term.py
+++ gif2term.py
@@ -2,76 +2,58 @@ import time
 import pygame as pg
 from PIL import Image
 import numpy as np
+import argparse
 import os
-import sys
+
+
+def clear_terminal():
+    print("\033[H", end='')
 
 
 def ascii_pixel(r: int, g: int, b: int, sign: str = " ") -> str:
-    """
-    Generates a string to display a pixel in the terminal with a specified background and character.
-
-    :param r: int - red color value (0-255)
-    :param g: int - green color value (0-255)
-    :param b: int - blue color value (0-255)
-    :param sign: str - character to be displayed (default is a space)
-    :return: str - string with ANSI code for colored background and character
-    """
     return f"\033[48;2;{r};{g};{b}m {sign} \033[0m"
 
 
-def compress_image(input_path, output_path, max_size):
-    with Image.open(input_path) as img:
-        img.thumbnail(max_size)
-        img.save(output_path)
+def compress_image_in_memory(image: Image.Image, max_size) -> Image.Image:
+    image.thumbnail(max_size)
+    return image
 
 
-def PrintImg2Terminal(image_path__: str, cell_mode: int = 0) -> str or None:
+def PrintImg2Terminal(image: Image.Image) -> str or None:
     term_picture: str = ""
 
-    if not os.path.exists(image_path__):
-        print(f"File not found: {image_path__}")
+    if image is None:
+        print("Invalid image provided.")
         return None
 
-    try:
-        img_array = np.array(Image.open(image_path__).convert("RGB"))
+    img_array = np.array(image.convert("RGB"))
 
-        sign_: str = " "
-        if cell_mode == 1:
-            sign_ = "."
-        elif cell_mode == 2:
-            sign_ = "+"
-        elif cell_mode == 3:
-            sign_ = "*"
-        elif cell_mode == 4:
-            sign_ = "x"
-        elif cell_mode == 5:
-            sign_ = "!"
+    for row in img_array:
+        term_picture += ''.join(ascii_pixel(r, g, b) for r, g, b in row) + "\n"
 
-        for row in img_array:
-            term_picture += f"{''.join(ascii_pixel(r, g, b, sign_) for r, g, b in row)}\n"
-        return term_picture
-    except Exception as e:
-        print(f"Error preparing the image: {e}")
-        return None
+    return term_picture
 
 
-def PrintGIF2Terminal(gif_path: str, max_size=(64, 64), cell_mode: int = 0) -> list[str] or None:
+def PrintGIF2Terminal(gif_path: str, max_size=(64, 64)) -> list[str] or None:
     frames = []
+
+    if not os.path.exists(gif_path):
+        print(f"File not found: {gif_path}")
+        return None
 
     with Image.open(gif_path) as gif:
         try:
             while True:
-                temp_frame_path = 'temp_frame.png'
-                gif.save(temp_frame_path)
 
-                compressed_frame_path = 'compressed_frame.png'
-                compress_image(temp_frame_path, compressed_frame_path, max_size)
+                frame = gif.copy()
 
-                terminal_representation = PrintImg2Terminal(compressed_frame_path, cell_mode)
+                compressed_frame = compress_image_in_memory(frame, max_size)
+
+                terminal_representation = PrintImg2Terminal(compressed_frame)
                 if terminal_representation is not None:
                     frames.append(terminal_representation)
-
                 gif.seek(gif.tell() + 1)
+
         except EOFError:
             pass
 
@@ -79,18 +61,25 @@ def PrintGIF2Terminal(gif_path: str, max_size=(64, 64), cell_mode: int = 0) -> l
 
 
 def main() -> int:
-    if len(sys.argv) != 6:
-        print(
-            f"Usage: python {sys.argv[0]} <path_for_gif> <path_for_music> <delay> <compress_size_x> <compress_size_y> ")
-        sys.exit(1)
-    else:
-        gif_path = sys.argv[1]
-        music_path = str(sys.argv[2]).upper()
-        delay = float(sys.argv[3])
-        compress_size_x = int(sys.argv[4])
-        compress_size_y = int(sys.argv[5])
+    parser = argparse.ArgumentParser(description="Show .gif file on terminal")
 
-    os.system("cls") if os.name == "nt" else os.system("clear")
+    parser.add_argument("gif_path", type=str, help="Путь к GIF файлу")
+    parser.add_argument("music_path", type=str, help="Путь к музыкальному файлу (или 'NONE' для отключения музыки)")
+    parser.add_argument("delay", type=float, help="Задержка между кадрами в секундах")
+    parser.add_argument("compress_size_x", type=int, help="Размер сжатия по оси X")
+    parser.add_argument("compress_size_y", type=int, help="Размер сжатия по оси Y")
+
+    args = parser.parse_args()
+
+    gif_path = args.gif_path
+    music_path = args.music_path.upper()
+    delay = args.delay
+    compress_size_x = args.compress_size_x
+    compress_size_y = args.compress_size_y
+
+    os.system("cls")
+
+    clear_terminal()
     print("Please, wait..")
 
     gif = PrintGIF2Terminal(gif_path, (compress_size_x, compress_size_y))
@@ -102,13 +91,14 @@ def main() -> int:
         pg.mixer.music.set_volume(0.15)
 
     while True:
-        os.system("cls") if os.name == "nt" else os.system("clear")
+        clear_terminal()
         for frame in gif:
             print(frame)
             if delay > 0:
                 time.sleep(delay)
-            os.system("cls") if os.name == "nt" else os.system("clear")
+            clear_terminal()
 
 
 if __name__ == "__main__":
     main()
+    os.system("cls")
